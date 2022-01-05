d2ef86fcb750c5f5e9d1fc38ef0851680610439d
xabi <ale23@hotmail.fr>
2022-01-05 15:14:47 +0100

    Day 1 part 2 done

--- src/com/adventofcode/day1/Solution.java
+++ src/com/adventofcode/day1/Solution.java
@@ -18,6 +18,7 @@ public class Solution {
 			}
 			scanner.close();
 			System.out.println(incNumber(inputs));
+			System.out.println(incWindow(inputs));
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -26,8 +27,21 @@ public class Solution {
 
 	public static int incNumber(List<String> inputs) {
 		int n = 0;
-		for (int i = 1; i < inputs.size(); i++)
-			n += Integer.parseInt(inputs.get(i)) > Integer.parseInt(inputs.get(i - 1)) ? 1 : 0;
+		for (int i = 0; i < inputs.size() - 1; i++)
+			n += Integer.parseInt(inputs.get(i + 1)) > Integer.parseInt(inputs.get(i)) ? 1 : 0;
+		return n;
+	}
+
+	public static int incWindow(List<String> inputs) {
+		int n = 0;
+		List<Integer> window = new ArrayList<Integer>();
+		for (int i = 0; i < inputs.size() - 2; i++) {
+			window.add(Integer.parseInt(inputs.get(i)) + Integer.parseInt(inputs.get(i + 1))
+					+ Integer.parseInt(inputs.get(i + 2)));
+			if (i > 0)
+				n += window.get(i) > window.get(i - 1) ? 1 : 0;
+		}
+
 		return n;
 	}
 }
